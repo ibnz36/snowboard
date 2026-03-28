0f2693613d443077aaa7eba206cc60de4e5e2ce0
ibnz36 <vicmdv@duck.com>
2026-03-28 03:06:22 +0100

    perf: prepare benchs

--- examples/perf-test/src/main.rs
+++ examples/perf-test/src/main.rs
@@ -1,6 +1,14 @@
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 use snowboard::{Result, Server};
 
-fn main() -> Result {
+#[smol_potat::main(threads = 12)]
+async fn main() -> Result {
 	// Not returning anything (`()`) is the same as Response::default()
-	Server::new("localhost:8080")?.run_async(|_| async {});
+	Server::new("localhost:8080")
+		.await?
+		.with_buffer_size(200)
+		.run(async |_| {})
+		.await;
 }
